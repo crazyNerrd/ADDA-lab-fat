594a0744dd7c2fe17f28c397b059c07175d94793
crazyNerrd <mitesh.ramteke2020@vitstudent.ac.in>
2024-04-25 12:37:46 +0530

    Update labfat.java file

--- labfat.java
+++ labfat.java
@@ -2,5 +2,7 @@ public class labfat {
     public static void main(String[] args)
     {
         System.out.println("Hello this is Mitesh Ramteke,20MIC0133");
+        System.out.println("ADDA LABFAT EXAM");
+
     }
 }
